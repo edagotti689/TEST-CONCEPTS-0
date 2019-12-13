752e35c22d3e4f27b1c74b0503cae15b06ed9b63
edagotti689 <53170802+edagotti689@users.noreply.github.com>
2019-12-14 00:25:48 +0530

    Add files via upload

---
+++ 4_Testing_Types.py
@@ -0,0 +1,13 @@
+url - https://www.guru99.com/smoke-sanity-testing.html
+
+
+Testing types 
+1.Smoke testing
+2.Sanity testing
+3.Regression testing
+4.Negative testing
+
+
+'''
+https://blog.testlodge.com/types-of-test-cases-in-software-testing/
+'''
---
+++ Testing_Concepts.py
@@ -0,0 +1,41 @@
+# SDLC - Software Development Life Cycle
+
+1. Requirement Gathering & Analysis
+2. Design
+3. Develop 
+4. Testing 
+5. Release 
+6. Maintainance 
+
+# STLC - Software Testing Life Cycle
+
+1. Requirement Analysis
+2. Test Planning (Test Strategy)
+3. Test Design
+4. Test Environment 
+5. Test Executing
+6. Test Closure
+
+# Testing Levels:
+
+1. Unit Testing
+2. Integration Testing
+3. System Testing
+4. User Acceptance Testing 
+
+1. Unit Testing: 
+= Developers will do the unit testing
+= testing each unit or component (component tests)
+
+2. Integration Testing:
+= Developers will do the integration testing 
+= Testing b/w two modules we can call it as Integration testing.
+Home --> to --> logout = Home to logout will do the integration testing 
+
+3.System Testing:
+= SYSTEM TESTING is a level of software testing where a complete and integrated software is tested. The purpose of this test is to evaluate the system's compliance with the specified requirements
+
+4. User Acceptance or client testing :
+
+User Acceptance Testing (UAT), also known as beta or end-user testing, is defined as testing the software by the user or client to determine whether it can be accepted or not. ... The main purpose of this testing is to validate the software against the business requirements
+
